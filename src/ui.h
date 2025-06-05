4e6b873dcdb0e5351cad2d88fa2da552975f3b05
Khadzhimurad <khad.gadzhiev@stud.unibas.ch>
2025-06-05 23:36:15 +0200

    Remove preliminary header files

--- src/ui.h
+++
@@ -1,8 +0,0 @@
-#ifndef UI_h
-#define UI_h
-// the overall UI header, this should ideally be used for functions calling things 
-// like our handler functions on startup or just constructing the UI in general
-
-void constructUI();
-
-#endif
